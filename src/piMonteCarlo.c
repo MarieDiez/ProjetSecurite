91045ad1da3d7555276c6b5e8668d1c202d47856
Eterniitys <johan.guerrero394@gmail.com>
2020-03-17 16:00:40 +0100

    update drawing area on nb_dot changed

--- src/piMonteCarlo.c
+++ src/piMonteCarlo.c
@@ -16,6 +16,9 @@ Point *lst_point;
 int nb_point = 1;
 int nb_point_lastdraw = 1;
 
+// Global GTK Object
+GtkDrawingArea * drawing_area;
+
 // GTK callback
 
 void _on_destroy_main_window(GtkButton *button, gpointer user_data){
@@ -29,31 +32,32 @@ void _on_spin_value_changed(GtkSpinButton *spin_button, gpointer user_data){
 void _on_calculate_clicked(GtkButton *button, gpointer user_data){
 	nb_point_lastdraw = nb_point;
 	call_monte_carlo();
+	gtk_widget_queue_draw((GtkWidget*)drawing_area);
 }
 
 gboolean draw_callback (GtkWidget *widget, cairo_t *cr, gpointer data){
 
 	guint width, height;
-	
+
 	GtkStyleContext *context;
 	context = gtk_widget_get_style_context (widget);
-	
+
 	width = gtk_widget_get_allocated_width (widget);
 	height = gtk_widget_get_allocated_height (widget);
-	
+
 	gtk_render_background(context, cr, 0, 0, width, height);
-	
-	
+
+
 	// drawing arc
 	cairo_set_line_width (cr, 2.0);
 	cairo_arc(
-		cr,
-		0,			// x
-		height,			// y
-		MIN (width, height),	// rayon
-		G_PI/2,			// angle1
-		0		// angle2
-	);
+			cr,
+			0,			// x
+			height,			// y
+			MIN (width, height),	// rayon
+			G_PI/2,			// angle1
+			0			// angle2
+		 );
 	cairo_stroke(cr);
 
 	cairo_set_line_width (cr, 1.0);
@@ -65,17 +69,17 @@ gboolean draw_callback (GtkWidget *widget, cairo_t *cr, gpointer data){
 				cairo_set_source_rgba (cr, 1, 0.2, 0.2, 0.6);
 			}
 			cairo_arc(
-				cr,
-				lst_point[i].x * MIN(width, height),
-				height - lst_point[i].y * MIN(width, height),
-				2,
-				0,
-				2 * G_PI
-			);
+					cr,
+					lst_point[i].x * MIN(width, height),
+					height - lst_point[i].y * MIN(width, height),
+					2,
+					0,
+					2 * G_PI
+				 );
 			cairo_fill (cr);
 		}
 	}
- return FALSE;
+	return FALSE;
 }
 
 // calcule
@@ -89,7 +93,7 @@ void call_monte_carlo(){
 			p.x =  rand()/(double)RAND_MAX;
 			p.y =  rand()/(double)RAND_MAX;
 			p.inner = 0;
-			
+
 			if (p.x*p.x + p.y*p.y <= 1){
 				cpt++;
 				p.inner = 1;
@@ -109,28 +113,29 @@ int main(int argc, char* argv[]){
 	srand(time(0));
 
 	GtkBuilder *builder;
-    GtkWidget *window;
+	GtkWidget *window;
 
 	// constructing window
-    gtk_init(&argc, &argv);
-    builder = gtk_builder_new();
-    gtk_builder_add_from_file(builder, "../monte_carlo.glade", NULL);
-    window = GTK_WIDGET(gtk_builder_get_object(builder, "monte_carlo"));
-    gtk_builder_connect_signals(builder, NULL);
-	
-	//GObjects
-    GtkSpinButton * btn_spin = (GtkSpinButton*) gtk_builder_get_object(builder, "nb_selector");
-    GtkDrawingArea * drawing_area = (GtkDrawingArea*) gtk_builder_get_object(builder, "drawing_area");
+	gtk_init(&argc, &argv);
+	builder = gtk_builder_new();
+	gtk_builder_add_from_file(builder, "../monte_carlo.glade", NULL);
+	window = GTK_WIDGET(gtk_builder_get_object(builder, "monte_carlo"));
+	gtk_builder_connect_signals(builder, NULL);
 
-    //signals
-    g_signal_connect(btn_spin, "value-changed", G_CALLBACK(_on_spin_value_changed), NULL);
+	// GObjects
+	GtkSpinButton * btn_spin = (GtkSpinButton*) gtk_builder_get_object(builder, "nb_selector");
+	// Global GObjects
+	drawing_area = (GtkDrawingArea*) gtk_builder_get_object(builder, "drawing_area");
+
+	// signals
+	g_signal_connect(btn_spin, "value-changed", G_CALLBACK(_on_spin_value_changed), NULL);
 	g_signal_connect (G_OBJECT (drawing_area), "draw", G_CALLBACK (draw_callback), NULL);
-	
-	// - - - 
-    g_object_unref(builder);
-    gtk_widget_show(window);                
-    gtk_main();
-    
+
+	// - - -
+	g_object_unref(builder);
+	gtk_widget_show(window);
+	gtk_main();
+
 	return EXIT_SUCCESS;
 } 
 
