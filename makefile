51965f051af633074917b6b250d2971e8995636f
Marie Diez <spel7900@gmail.com>
2020-03-20 11:52:13 +0100

    add gradient Descent gtk

--- makefile
+++ makefile
@@ -1,10 +1,10 @@
 all :
 	cd src && gcc -rdynamic `pkg-config --cflags gtk+-3.0` piMonteCarlo.c -o ../build/piMonteCarlo `pkg-config --libs gtk+-3.0`
+	cd src && gcc -rdynamic `pkg-config --cflags gtk+-3.0` gradientDescentGTK.c -o ../build/gradientDescentGTK `pkg-config --libs gtk+-3.0`
 	gcc src/date.c -o build/date
 	gcc src/inertieIntraClasse.c -o build/inertieIntraClasse
 	gcc src/piece.c -o build/piece
-	gcc src/gradientDescent.c -o build/gradientDescent
-
+	
 mediaplayer :
 	cd src && gcc -rdynamic `pkg-config --cflags gtk+-3.0` mediaplayer.c -o ../build/mediaplayer `pkg-config --libs gtk+-3.0`
 	
--- src/gradientDescentGTK.c
+++ src/gradientDescentGTK.c
@@ -4,44 +4,67 @@
 #include <math.h>
 #include <gtk/gtk.h>
 
-/* y =ax +b */
+void gradientDescent();
+
 typedef struct _Point{
 	double x;
 	double y;
-	int inner;
 } Point;
+
+Point *lst_point;
+int m = 1;
+int nb_point_lastdraw = 1;
+double coeffa;
+double coeffb;
+GtkDrawingArea * drawing_area;
 
 
 void _on_destroy_main_window(GtkButton *button, gpointer user_data){
 	gtk_main_quit();
 }
 
+void _on_spin_value_changed(GtkSpinButton *spin_button, gpointer user_data){
+	m =  gtk_spin_button_get_value_as_int (spin_button);
+}
+
 void _on_calculate_clicked(GtkButton *button, gpointer user_data){
-	nb_point_lastdraw = nb_point;
+	nb_point_lastdraw = m;
 	gradientDescent();
+	gtk_widget_queue_draw((GtkWidget*)drawing_area);
+}
+
+double function(double a,double b, double x){
+	return a*x+b;
 }
 
 gboolean draw_callback (GtkWidget *widget, cairo_t *cr, gpointer data){
-
 	guint width, height;
-	
 	GtkStyleContext *context;
 	context = gtk_widget_get_style_context (widget);
-	
 	width = gtk_widget_get_allocated_width (widget);
 	height = gtk_widget_get_allocated_height (widget);
-	
 	gtk_render_background(context, cr, 0, 0, width, height);
+	cairo_set_line_width (cr, 2.0);
+	cairo_set_source_rgba (cr, 0, 0, 0.5, 1);
+	cairo_move_to(cr,0.0, height - function(coeffa,coeffb*MIN(width, height),0));
+	cairo_line_to(cr,width, height - function(coeffa,coeffb*MIN(width, height),width));
+	cairo_stroke(cr);
 
-	cairo_set_line_width (cr, 1.0);
-		if (lst_point){
-			for(int i = 0 ; i < nb_point_lastdraw ; i++){
-
-				cairo_set_source_rgba (cr, 1, 0.2, 0.2, 0.6);
-				
-			}
+	if (lst_point){
+		for(int i = 0 ; i < nb_point_lastdraw ; i++){
+			cairo_set_source_rgba (cr, 1, 0.2, 0.2, 0.6);
+			cairo_arc(
+					cr,
+					lst_point[i].x * MIN(width, height),
+					height - lst_point[i].y * MIN(width, height),
+					2,
+					0,
+					2 * G_PI
+				 );
+			cairo_fill (cr);
 		}
-	 return FALSE;
+	}
+	return FALSE;
 }
 
 double model(double x){
@@ -55,7 +78,6 @@ double carre(double number){
 double gradientA(double a, double b, double (*model)(double), double m, Point * lst_point){
 	double somme = 0;
 	for (int i = 0; i < m; i++){
-		//somme +=  model(lst_point[i].x) * (a * model(lst_point[i].x) + b - lst_point[i].y);
 		somme +=  lst_point[i].x * (a * lst_point[i].x + b - lst_point[i].y);
 	}
 	return (1/m)*somme;
@@ -64,117 +86,57 @@ double gradientA(double a, double b, double (*model)(double), double m, Point * 
 double gradientB(double a, double b, double (*model)(double), double m, Point * lst_point){
 	double somme = 0;
 	for (int i = 0; i < m; i++){
-		//somme +=  (a * model(lst_point[i].x) + b - lst_point[i].y);
 		somme +=  (a * lst_point[i].x + b - lst_point[i].y);
 	}
 	return (1/m)*somme;
 }
 
-double[] gradientDescent(){
-	double m = 10;
-	Point *lst_point = malloc(m * sizeof(Point));
-
-	/*for (int i = 0; i < m; i++){
+void gradientDescent(){
+	lst_point = malloc(m * sizeof(Point));
+	for (int i = 0; i < m; i++){
 		Point p;
-		p.x =  rand()%10;
-		p.y =  rand()%10;
+		p.x =  rand()/(double)RAND_MAX;
+		p.y =  rand()/(double)RAND_MAX;
 		lst_point[i] = p;
-		printf("%.2f %.2f\n",p.x,p.y);
 	}
-
-	double a = rand()%1000;
-	double b = rand()%1000;*/
-
-	Point p;
-	p.x = 2;
-	p.y = 1;
-	lst_point[0] = p;
-	Point p1;
-	p1.x = 2;
-	p1.y = 3;
-	lst_point[1] = p1;
-	Point p2;
-	p2.x = 4;
-	p2.y = 2;
-	lst_point[2] = p2;
-	Point p3;
-	p3.x = 3;
-	p3.y = 5;
-	lst_point[3] = p3;
-	Point p4;
-	p4.x = 7;
-	p4.y = 3;
-	lst_point[4] = p4;
-	Point p5;
-	p5.x = 6;
-	p5.y = 7;
-	lst_point[5] = p5;
-	Point p6;
-	p6.x = 8;
-	p6.y = 4;
-	lst_point[6] = p6;
-	Point p7;
-	p7.x = 8;
-	p7.y = 6;
-	lst_point[7] = p7;
-	Point p8;
-	p8.x = 4;
-	p8.y = 6;
-	lst_point[8] = p8;
-	Point p9;
-	p9.x = 7;
-	p9.y = 5;
-	lst_point[9] = p9;
-
-	double a = 10;
-	double b = 7;
-
-	printf("%.2f %.2f\n",a,b);
+	double a = rand()/(double)RAND_MAX;
+	double b = rand()/(double)RAND_MAX;
 	double learningRate = 0.001;
-
 	double epsilon = 0.5;
 	int k = 0;
 	double diff = 1;
 	double olda;
 	double oldb;
-
 	while(fabs(olda-a) != 0 && fabs(oldb-b) != 0){
 		olda = a;
 		oldb = b;
 		a = a - learningRate * gradientA(a,b,model,m,lst_point);
 		b = b - learningRate * gradientB(a,b,model,m,lst_point);
 	}
-	double coeff[2];
-	coeff[0] = a;
-	coeff[1] = b;
-
-	return coeff;
+	coeffa = a; 
+	coeffb = b;
 }
 
-double main(){
-	srand(time(0));
+int main(int argc, char* argv[]){
 
+	srand(time(0));
 	GtkBuilder *builder;
     GtkWidget *window;
-
 	// constructing window
     gtk_init(&argc, &argv);
     builder = gtk_builder_new();
     gtk_builder_add_from_file(builder, "../gradient_descent.glade", NULL);
     window = GTK_WIDGET(gtk_builder_get_object(builder, "gradient_descent"));
     gtk_builder_connect_signals(builder, NULL);
-	
-	//GObjects
-    GtkDrawingArea * drawing_area = (GtkDrawingArea*) gtk_builder_get_object(builder, "drawing_area");
-
-    //signals
+	// GObjects
+	GtkSpinButton * btn_spin = (GtkSpinButton*) gtk_builder_get_object(builder, "nb_selector");
+	// Global GObjects
+	drawing_area = (GtkDrawingArea*) gtk_builder_get_object(builder, "drawing_area");
+	// signals
+	g_signal_connect(btn_spin, "value-changed", G_CALLBACK(_on_spin_value_changed), NULL);
 	g_signal_connect (G_OBJECT (drawing_area), "draw", G_CALLBACK (draw_callback), NULL);
-	
-	// - - - 
-    g_object_unref(builder);
-    gtk_widget_show(window);                
-    gtk_main();
-
-	
+	g_object_unref(builder);
+	gtk_widget_show(window);
+	gtk_main();
 	return EXIT_SUCCESS;
 }
